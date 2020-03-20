f33a3bb86604eaa7bd01ae6ed1053c553612a47c
Adrian Albanese <adrian.albanese@troteclaser.com.au>
2020-03-20 16:18:06 +1100

    Updated GUI and implemented some IP camera connection functions

--- Sample Camera Stream/MainWindow.xaml.cs
+++ Sample Camera Stream/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Ozeki.Media;
 using Ozeki.Camera;
 
@@ -22,58 +10,86 @@ namespace Sample_Camera_Stream
     /// </summary>
     public partial class MainWindow : Window
     {
-        private VideoViewerWPF _videoViewerWPF;
-        private DrawingImageProvider _provider;
+        private VideoViewerWPF _videoViewer;
+        private IWebCamera _webCamera;
         private IIPCamera _ipCamera;
-        private WebCamera _webCamera;
-        private MediaConnector _connector;
+        private DrawingImageProvider _imageProvider;
+        private MediaConnector _mediaConnector;
 
         public MainWindow()
         {
             InitializeComponent();
-            _connector = new MediaConnector();
-            _provider = new DrawingImageProvider();
+            _videoViewer = new VideoViewerWPF();
+            _imageProvider = new DrawingImageProvider();
+            _mediaConnector = new MediaConnector();
+            _webCamera = new WebCamera();
+
             SetVideoViewer();
+            UpdateUSBCameraList();
+        }
+
+        private void UpdateUSBCameraList()
+        {
+            if (_webCamera != null)
+            {
+                CameraList.Items.Add(_webCamera.DeviceName);
+                CameraList.SelectedIndex = 0;
+            }
         }
 
         private void SetVideoViewer()
         {
-            _videoViewerWPF = new VideoViewerWPF
+            _videoViewer = new VideoViewerWPF
             {
                 HorizontalAlignment = HorizontalAlignment.Stretch,
                 VerticalAlignment = VerticalAlignment.Stretch,
                 Background = Brushes.Black
             };
-            CameraBox.Children.Add(_videoViewerWPF);
-            _videoViewerWPF.SetImageProvider(_provider);
+            CameraBox.Children.Add(_videoViewer);
+            _videoViewer.SetImageProvider(_imageProvider);
         }
 
         private void ConnectUSBCamera_Click(object sender, RoutedEventArgs e)
         {
-            _webCamera = new WebCamera();
 
-            if(_webCamera != null)
-            {
-                _connector.Connect(_webCamera.VideoChannel, _provider);
-                _videoViewerWPF.SetImageProvider(_provider);
-                _webCamera.Start();
-                _videoViewerWPF.Start();
-            }
+            _webCamera = new WebCamera();
+            _mediaConnector.Connect(_webCamera.VideoChannel, _imageProvider);
+            _videoViewer.SetImageProvider(_imageProvider);
+            _webCamera.Start();
+            _videoViewer.Start();
         }
 
         private void DisconnectUSBCamera_Click(object sender, RoutedEventArgs e)
         {
+            _videoViewer.Stop();
+            _webCamera.Stop();
+            _mediaConnector.Disconnect(_webCamera.VideoChannel, _imageProvider);
+            _webCamera = null;
 
         }
 
         private void ConnectIPCamera_Click(object sender, RoutedEventArgs e)
         {
+            var host = HostAddress.Text;
+            var port = HostPort.Text;
+            var user = HostUserName.Text;
+            var pass = HostPassword.Password;
 
+            _ipCamera = IPCameraFactory.GetCamera(host + ":" + port, user, pass);
+            if (_ipCamera == null) { statusBar.Content = "No Camera Found!"; return; }
+
+            _mediaConnector.Connect(_ipCamera.VideoChannel, _imageProvider);
+            _videoViewer.SetImageProvider(_imageProvider);
+            _ipCamera.Start();
+            _videoViewer.Start();
         }
 
         private void DisonnectIPCamera_Click(object sender, RoutedEventArgs e)
         {
-
+            _videoViewer.Stop();
+            _ipCamera.Disconnect();
+            _ipCamera.Dispose();
+            _mediaConnector.Disconnect(_ipCamera.VideoChannel, _imageProvider);
         }
     }
 }
